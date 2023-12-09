e879f8630070bcc63c4b8ada3ef09a3d4b125bea
TheRealRomy <petracorta.rmnl@gmail.com>
2023-12-09 18:20:51 +0800

    Added the coyote time and jump buffer to the OG script (Own script)
    Also fixed the bug when the player presses the move input and jumps towards a wall, it'll not stick anymore
    
    Fix: Create a Physics Material 2D and set the friction of the rigid body of the player to 0 so that it'll not stick anymore.

--- Assets/Scripts/Player Movement/Movement.cs
+++ Assets/Scripts/Player Movement/Movement.cs
@@ -17,6 +17,8 @@ namespace romnoelp
         private float coyoteTimeCounter;
         [SerializeField] private float jumpBufferTime = .2f;
         private float jumpBufferCounter; 
+        private bool isFacingRight = true;
+
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
@@ -26,16 +28,63 @@ namespace romnoelp
         private void Update() 
         {
             horizontalMovement = Input.GetAxisRaw("Horizontal");
+            
+            // Coyote timer (Coyote -> The amount of time the player can press space after leaving the ground)
+            if (IsGrounded()) 
+            {
+                coyoteTimeCounter = coyoteTime;
+                Debug.Log("Grounded");
+            }
+            else
+            {
+                coyoteTimeCounter -= Time.deltaTime;
+            }
+
+            if(Input.GetButtonDown("Jump"))
+            {
+                jumpBufferCounter = jumpBufferTime;
+            }
+            else
+            {
+                jumpBufferCounter -= Time.deltaTime;
+            }
+
+            if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x,  jumpForce);
+                jumpBufferCounter = 0f;
+            }
+
+            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
+                coyoteTimeCounter = 0f;
+            }
         }
 
+
+        // Physics for the movement
         private void FixedUpdate()
         {
             rb.velocity = new Vector2(horizontalMovement * movementSpeed, rb.velocity.y);
         }
 
+        // Creates a box cast, a collider below the player, and will return true if it detects a collision between the specified layer 
         private bool IsGrounded() 
         {
-            return Physics2D.BoxCast(playerBoxcastCollider.bounds.center, playerBoxcastCollider.bounds.size, 0f, Vector2.down, 1f, ground);
+            return Physics2D.BoxCast(playerBoxcastCollider.bounds.center, playerBoxcastCollider.bounds.size, 0f, Vector2.down, .1f, ground);
+        }
+
+        // Flips the character when the horizontal movement becomes negative (Note: On a plane, negative <---- 0 ----> positive)
+        private void Flip()
+        {
+            if (isFacingRight && horizontalMovement < 0f || !isFacingRight && horizontalMovement > 0f)
+            {
+                Vector3 localScale = transform.localScale;
+                isFacingRight = !isFacingRight;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
         }
     }
 
